ef6f65248b452626c03b830268aa622e8449bc91
TheLastChance23 <chijevsky.maxim233@gmail.com>
2020-10-21 22:46:44 +0300

    Запуск проекта

---
+++ OurWork (LastChance_VItex).cpp
@@ -0,0 +1,25 @@
+// Здесь начнётся наше розвитие в компьютерной индустрии. Да прибудет с нами СИЛА И СТРЕМЛЕНИЕ!
+// Спасибо всем за помощь в проекте. 
+
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 5; ++i)
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << setw(56) << "{TheLastChance and Vitex}" << endl;
+	system("color 0E");
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
