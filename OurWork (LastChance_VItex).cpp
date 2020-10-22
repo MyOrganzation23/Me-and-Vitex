4a452574d9c98de1f96c97ec46a655f41e2dd983
TheLastChance23 <chijevsky.maxim233@gmail.com>
2020-10-22 13:22:36 +0300

    Добавлен ccp файл для будущей работы в нём

---
+++ OurWork (LastChance_VItex).cpp
@@ -0,0 +1,26 @@
+// Здесь начнётся наше розвитие в програмной индустрии. Да прибудет с нами СИЛА И СТРЕМЛЕНИЕ!
+// Спасибо всем за помощь в проекте. 
+
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+#include "string"
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << setw(56) << "{TheLastChance and Vitex}" << endl;
+	system("color 0E");
+	
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
